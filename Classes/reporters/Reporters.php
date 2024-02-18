c1e3bd1dfaf7bfa886160b0e110b5d3adc13f5dc
Islay Anderson <islayanderson@outlook.com>
2024-02-18 20:46:24 +0000

    setting up the core concepts that will be used

---
+++ Classes/reporters/Reporters.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace reporters;
+
+class Reporters{
+    //author: islay
+    //music: Sub Focus and Dimension at Printworks Weekend X
+    //tea: Yorkshire
+    private $example_data = '/example_data/reporters.xml';
+    private $default_data = '/data_xml/reporters.xml';
+    private $data_loc;
+    private $data;
+
+    public function __construct(Type $data_in = null) {
+        switch($data_in){
+            case "example": $this->data_loc = $this->example_data;
+            case "": $this->data_loc = $this->default_data;
+            default: $this->data_loc = $data_in;
+        }
+
+        $this->data = simplexml_load_file(__DIR__."/../..".$this->data_loc);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ComponentWatch
+
+# Setup
+## Database
+configure database in .env
+'DB_HOST'
+'DB_USER'
+'DB_PASS'
+'DB_SCHEMA'
+no defaults are assumed 
