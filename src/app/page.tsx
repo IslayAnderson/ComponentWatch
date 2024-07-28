18d0e42962b9994801418d08ace6b6954990b7f4
Islay Anderson <islayanderson@outlook.com>
2024-07-29 00:24:57 +0100

    follow the development button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,15 @@ export default function Home() {
                   <h1 className="text-gray-900 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">Unintrusive cross-platform solution to component analytics.</h1>
                   <p className="mt-8 text-white">componentWatch simplifies the process of configuring component analytics by abstracting your analytics away from everything under the hood empowing your content editors build optimised web pages</p>
                   <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+                  <a
+                        href="https://github.com/IslayAnderson/ComponentWatch"
+                        className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-blue-600 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+                      >
+                        <span
+                          className="relative text-base font-semibold text-primary dark:text-white"
+                          >Follow the Development</span
+                        >
+                      </a>
                       <a
                         href="mailto:hello@componentwatch.tech?subject=Registering%20my%20interest%20&body=I'm%20interested%20for%20a%20demo%20of%20componentWatch"
                         className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
