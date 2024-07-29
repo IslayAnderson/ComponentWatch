6a27832ecb4960da197b8c4f8707935a5e327a2b
Islay Anderson <islayanderson@outlook.com>
2024-07-29 17:30:10 +0100

    update content of email button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
                         >
                       </a>
                       <a
-                        href="mailto:hello@componentwatch.tech?subject=Registering%20my%20interest%20&body=I'm%20interested%20for%20a%20demo%20of%20componentWatch"
+                        href="mailto:hello@componentwatch.tech?subject=Registering%20my%20interest%20&body=I'm%20interested%20in%20the%20a%20demo%20of%20componentWatch"
                         className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
                       >
                         <span
